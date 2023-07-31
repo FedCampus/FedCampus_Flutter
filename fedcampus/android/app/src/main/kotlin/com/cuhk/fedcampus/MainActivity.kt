cc02ef71b7d7974d4c831036065311448ae5570e
Beilong-Tang <bt132@duke.edu>
2023-07-31 15:19:56 +0800

    error

--- fedcampus/android/app/src/main/kotlin/com/cuhk/fedcampus/MainActivity.kt
+++ fedcampus/android/app/src/main/kotlin/com/cuhk/fedcampus/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cuhk.fedcampus
 
 import io.flutter.embedding.android.FlutterActivity
-
+import com.cuhk.fedcampus.health.utils.exercisedata.getExerciseData
 class MainActivity: FlutterActivity() {
+
 }
