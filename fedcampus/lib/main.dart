ce502611c67b8b4560382840bdd18f13dbc0f0e5
Luyao Wang <lywlywly@gmail.com>
2023-08-02 00:00:14 +0800

    minor issue in formatting

--- fedcampus/lib/main.dart
+++ fedcampus/lib/main.dart
@@ -9,16 +9,15 @@ void main() {
     theme: ThemeData(
       useMaterial3: false,
       colorScheme: ColorScheme.fromSeed(
-        seedColor: const Color.fromARGB(255, 229, 85, 85),
-        primary: Colors.black,
-        primaryContainer: const Color.fromARGB(255, 229, 85, 85),
-        secondary: const Color.fromARGB(255, 217, 217, 217),
-        tertiary: const Color.fromARGB(51, 0, 0, 0),
-        shadow: const Color.fromARGB(38, 229, 85, 85),
-        outline: const Color.fromARGB(25, 0, 0, 0),
-        surfaceTint: const Color.fromARGB(255, 206, 229, 108),
-        tertiaryContainer: const Color.fromARGB(225, 131, 139, 217)
-      ),
+          seedColor: const Color.fromARGB(255, 229, 85, 85),
+          primary: Colors.black,
+          primaryContainer: const Color.fromARGB(255, 229, 85, 85),
+          secondary: const Color.fromARGB(255, 217, 217, 217),
+          tertiary: const Color.fromARGB(51, 0, 0, 0),
+          shadow: const Color.fromARGB(38, 229, 85, 85),
+          outline: const Color.fromARGB(25, 0, 0, 0),
+          surfaceTint: const Color.fromARGB(255, 206, 229, 108),
+          tertiaryContainer: const Color.fromARGB(225, 131, 139, 217)),
     ),
     home: const HomeRoute(),
   ));
