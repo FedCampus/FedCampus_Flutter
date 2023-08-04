0dc309fb79e2b89846ac19d7782096d16e820a91
Luyao Wang <lywlywly@gmail.com>
2023-08-05 02:34:29 +0800

    fix python formatting

--- test_fa_backend/test_fa_backend/health_stats/apps.py
+++ test_fa_backend/test_fa_backend/health_stats/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HealthStatsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'health_stats'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "health_stats"
--- test_fa_backend/test_fa_backend/test_fa_backend/urls.py
+++ test_fa_backend/test_fa_backend/test_fa_backend/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('health_stats.urls'))
+    path("admin/", admin.site.urls),
+    path("api/", include("health_stats.urls")),
 ]
