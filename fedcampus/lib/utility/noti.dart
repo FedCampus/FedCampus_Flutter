a55577de98d22adcf4b1b95f84a45902ab79b65d
Beilong-Tang <bt132@duke.edu>
2023-08-27 09:58:07 +0800

    notification sample code done

--- fedcampus/lib/utility/noti.dart
+++ fedcampus/lib/utility/noti.dart
@@ -1,5 +1,7 @@
 // Notification class
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 class Noti {
   static Future initialize(
@@ -12,22 +14,37 @@ class Noti {
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 
+  static tz.TZDateTime _nextInstanceOfTenAM() {
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate =
+        tz.TZDateTime(tz.local, now.year, now.month, now.day, 9, 52);
+    if (scheduledDate.isBefore(now)) {
+      scheduledDate = scheduledDate.add(const Duration(days: 1));
+    }
+    return scheduledDate;
+  }
+
   static Future showBigTextNotification(
       {var id = 0,
       required String title,
       required String body,
       var payload,
       required FlutterLocalNotificationsPlugin fln}) async {
-    AndroidNotificationDetails androidPlatformChannelSpecifics =
-        const AndroidNotificationDetails(
-      "1",
-      "1",
-      playSound: true,
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-
-    var not = NotificationDetails(android: androidPlatformChannelSpecifics);
-    await fln.show(0, title, body, not);
+    tz.initializeTimeZones();
+    tz.setLocalLocation(tz.getLocation("Asia/Hong_Kong"));
+    fln.zonedSchedule(
+        id,
+        title,
+        body,
+        _nextInstanceOfTenAM(),
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+              'TrainNotification', 'TrainNotification',
+              channelDescription: 'TrainNotification'),
+        ),
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time);
+    // await fln.show(0, title, body, not);
   }
 }
