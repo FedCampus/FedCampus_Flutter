cb478a90e185d47bc472f5ac8f5017b9280c5986
Luyao Wang <lywlywly@gmail.com>
2023-09-01 13:21:22 +0800

    change splash screen UI

--- fedcampus/lib/view/splash.dart
+++ fedcampus/lib/view/splash.dart
@@ -1,8 +1,11 @@
+import 'package:fedcampus/main.dart';
 import 'package:fedcampus/models/datahandler/health_factory.dart';
 import 'package:fedcampus/models/user_api.dart';
+import 'package:fedcampus/view/me.dart';
 import 'package:fedcampus/view/me/signin.dart';
 import 'package:fedcampus/view/navigator.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -13,16 +16,17 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   HealthDataHandlerFactory healthFactory = HealthDataHandlerFactory();
+  String serviceProvider = "huawei";
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      detectHealthDataServiceProvider().then((value) => Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const BottomNavigator()),
-          ));
-    });
+    // WidgetsBinding.instance.addPostFrameCallback((_) {
+    //   detectHealthDataServiceProvider().then((value) => Navigator.push(
+    //         context,
+    //         MaterialPageRoute(builder: (context) => const BottomNavigator()),
+    //       ));
+    // });
   }
 
   void detectFirstTimeLogin() async {
@@ -35,6 +39,17 @@ class _SplashState extends State<Splash> {
         );
       }
     }
+  }
+
+  void init() {
+    serviceProvider = userApi.prefs.getString("service_provider") ?? "huawei";
+    userApi.healthDataHandler =
+        healthFactory.creatHealthDataHandler(serviceProvider);
+  }
+
+  toggleTheme(bool b) async {
+    userApi.prefs.setBool('isDarkModeOn', b);
+    Provider.of<MyAppState>(context, listen: false).toggleTheme(b);
   }
 
   Future<void> detectHealthDataServiceProvider() async {
@@ -78,9 +93,172 @@ class _SplashState extends State<Splash> {
     }
   }
 
+  Future<void> _pickServiceProvider() async {
+    await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        double pixel = MediaQuery.of(context).size.width / 400;
+        return AlertDialog(
+          title: const Text("Select a health serivice provider"),
+          contentPadding:
+              EdgeInsets.fromLTRB(13 * pixel, 15 * pixel, 13 * pixel, 0),
+          content: SizedBox(
+              height: 271 * pixel,
+              width: 300 * pixel,
+              child: Column(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        userApi.prefs.setString("service_provider", "huawei");
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Huawei Health")),
+                  TextButton(
+                      onPressed: () {
+                        userApi.prefs.setString("service_provider", "google");
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Google Fit"))
+                ],
+              )),
+        );
+      },
+    );
+  }
+
+  Future<void> _chooseColorMode() async {
+    await showDialog<bool>(
+        context: context,
+        builder: (context) {
+          double pixel = MediaQuery.of(context).size.width / 400;
+          return AlertDialog(
+            title: const Text("Select color mode"),
+            contentPadding:
+                EdgeInsets.fromLTRB(13 * pixel, 15 * pixel, 13 * pixel, 0),
+            content: SizedBox(
+                height: 271 * pixel,
+                width: 300 * pixel,
+                child: Column(
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          toggleTheme(false);
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("light")),
+                    TextButton(
+                        onPressed: () {
+                          toggleTheme(true);
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("dark"))
+                  ],
+                )),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    double pixel = MediaQuery.of(context).size.width / 400;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            Text("Welcome to",
+                style: TextStyle(
+                  fontFamily: 'Montserrat Alternates',
+                  fontSize: 30 * pixel,
+                  color: Theme.of(context).colorScheme.onTertiaryContainer,
+                )),
+            const Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            Image.asset(
+              'assets/images/title.png',
+              color: Theme.of(context).colorScheme.onTertiaryContainer,
+              height: 60 * pixel,
+            ),
+            const Expanded(
+              flex: 3,
+              child: SizedBox(),
+            ),
+            ClipOval(
+              child: Stack(
+                children: [
+                  Container(
+                    width: 145 * pixel,
+                    height: 145 * pixel,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.tertiaryContainer,
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/images/me_nav_icon.png',
+                    width: 145 * pixel,
+                    height: 145 * pixel,
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+              flex: 3,
+              child: SizedBox(),
+            ),
+            const MeDivider(),
+            MeText(
+              text: 'Data Provider',
+              callback: _pickServiceProvider,
+            ),
+            const MeDivider(),
+            MeText(
+              text: 'Color Mode',
+              callback: _chooseColorMode,
+            ),
+            const MeDivider(),
+            TextButton(
+              onPressed: () {
+                {
+                  init();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BottomNavigator()),
+                  );
+                }
+              },
+              child: Text(
+                'click to continue',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onTertiaryContainer,
+                  fontSize: pixel * 18,
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 3,
+              child: SizedBox(),
+            ),
+            Text(
+              'Powered by DKU EdgeIntelligence',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onTertiaryContainer,
+                fontSize: pixel * 18,
+              ),
+            ),
+            const Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
