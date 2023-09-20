1a3d548e4125a290757628d8ce1ee4c90caf8b9f
Luyao Wang <lywlywly@gmail.com>
2023-09-21 03:05:55 +0800

    integrated app usage stats into the android native code

--- fedcampus/android/app/src/main/kotlin/com/cuhk/fedcampus/pigeon/AppUsageStatsClass.kt
+++ fedcampus/android/app/src/main/kotlin/com/cuhk/fedcampus/pigeon/AppUsageStatsClass.kt
@@ -2,27 +2,99 @@ package com.cuhk.fedcampus.pigeon
 
 import AppUsageStats
 import Data
+import android.app.Activity
+import android.app.AppOpsManager
+import android.app.usage.UsageEvents
+import android.app.usage.UsageStatsManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Process
+import android.provider.Settings
 import android.util.Log
-import com.cuhk.fedcampus.MainActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
-class AppUsageStatsClass(activity: MainActivity) : AppUsageStats {
-    val activity: MainActivity
+class AppUsageStatsClass(activity: Activity) : AppUsageStats {
+  val activity: Activity
 
-    init {
-        this.activity = activity
+  init {
+    this.activity = activity
+  }
+
+  override fun getData(
+      name: String,
+      startTime: Long,
+      endTime: Long,
+      callback: (Result<List<Data>>) -> Unit,
+  ) {
+    val scope = MainScope()
+    Log.i("APP", "hello")
+    getAppUsage()
+    val data = listOf(Data("test", 1.0, 1, 1))
+    scope.launch { callback(Result.success(data)) }
+  }
+
+  private fun getAppUsage() {
+    Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS).apply {
+      activity.startActivity(
+          activity.intent,
+      )
+    }
+    if (checkUsageStatsPermission()) {
+      // Implement further app logic here
+    } else {
+      // Navigate the user to the permission settings
+      Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS).apply {
+        activity.startActivity(
+            this,
+        )
+      }
     }
 
-    override fun getData(
-            name: String,
-            startTime: Long,
-            endTime: Long,
-            callback: (Result<List<Data>>) -> Unit
-    ) {
-        val scope = MainScope()
-        Log.i("APP", "hello")
-        val data = listOf(Data("test", 1.0, 1, 1))
-        scope.launch { callback(Result.success(data)) }
+    var foregroundAppPackageName: String? = null
+    val currentTime = System.currentTimeMillis()
+    // The `queryEvents` method takes in the `beginTime` and `endTime` to retrieve the usage
+    // events.
+    // In our case, beginTime = currentTime - 10 minutes ( 1000 * 60 * 10 milliseconds )
+    // and endTime = currentTime
+    val usageStatsManager =
+        activity.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+
+    Log.e("APP", currentTime.toString())
+
+    val usageEvents = usageStatsManager.queryUsageStats(0, 1693065600000, currentTime)
+    val usageEvent = UsageEvents.Event()
+
+    for (e in usageEvents) {
+      Log.e(
+          "App",
+          "${e.packageName} ${e.lastTimeUsed} ${e.firstTimeStamp} ${e.lastTimeStamp} ${e.totalTimeInForeground / 60000} ${e.totalTimeVisible / 60000}"
+      )
     }
+  }
+
+  private fun checkUsageStatsPermission(): Boolean {
+    val appOpsManager =
+        activity.getSystemService(AppCompatActivity.APP_OPS_SERVICE) as AppOpsManager
+    // `AppOpsManager.checkOpNoThrow` is deprecated from Android Q
+    val mode =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          appOpsManager.unsafeCheckOpNoThrow(
+              "android:get_usage_stats",
+              Process.myUid(),
+              activity.packageName
+          )
+        } else {
+          appOpsManager.checkOpNoThrow(
+              "android:get_usage_stats",
+              Process.myUid(),
+              activity.packageName
+          )
+        }
+    Log.e("App", mode.toString())
+    return mode == AppOpsManager.MODE_ALLOWED
+  }
 }
