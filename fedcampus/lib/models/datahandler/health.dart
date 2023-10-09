05f75c79e486ac48e371314b2f133bd86737db15
Luyao Wang <lywlywly@gmail.com>
2023-10-09 17:39:40 +0800

    new implementation of cached health data in db

--- fedcampus/lib/models/datahandler/health.dart
+++ fedcampus/lib/models/datahandler/health.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 import 'package:fedcampus/pigeon/generated.g.dart';
 import '../../utility/global.dart';
+import '../../utility/health_database.dart';
 import '../../utility/log.dart';
+import '../../utility/calendar.dart' as calendar;
 
 class FedHealthData {
   /// abstract class for health data controller on variery of platforms such as Huawei Health, Google Fit
@@ -73,9 +75,10 @@ class FedHealthData {
     return dataMap;
   }
 
-  Future<Map<String, double>> getCachedBodyData(
+  Future<Map<String, double>> getCachedBodyDataLegacy(
       DateTime dateTime, List<String> dataList,
       {bool forcedRefresh = false}) async {
+    // deprecated, old implementation which uses [SharedPreferences]
     Map<String, double> healthData;
     String? cachedData = userApi.prefs.getString("health$dateTime");
     if (cachedData != null) {
@@ -96,6 +99,57 @@ class FedHealthData {
           startTime: dateTime,
           endTime: dateTime.add(const Duration(days: 1)));
     }
+    return healthData;
+  }
+
+  Future<Map<String, double>> getCachedBodyData(
+      DateTime dateTime, List<String> dataList,
+      {bool forcedRefresh = false}) async {
+    HealthDatabase healthDatabase = await HealthDatabase.create();
+
+    Map<String, double> healthData = {};
+    List<String> dirtyDataList = [];
+
+    if (forcedRefresh) {
+      dirtyDataList = dataList;
+    } else {
+      // logger.d(await healthDatabase.getDataList());
+      // try to get data from DB, if < 30 min
+      for (var i in dataList) {
+        List<Map<String, Object?>> result =
+            await healthDatabase.getData(calendar.dateTimeToInt(dateTime), i);
+        if (result.isEmpty) {
+          dirtyDataList.add(i);
+        } else if ((DateTime.now().millisecondsSinceEpoch -
+                (result[0]["time_modified"] as int)) <
+            1800000) {
+          healthData.addAll({i: result[0]["value"] as double});
+        } else {
+          logger.e(i);
+          dirtyDataList.add(i);
+        }
+      }
+    }
+
+    Map<String, double> additionalHealthData = await getDataMap(
+        entry: dirtyDataList,
+        startTime: dateTime,
+        endTime: dateTime.add(const Duration(days: 1)));
+
+    // write newly queried data into DB
+    for (final e in additionalHealthData.entries) {
+      healthDatabase.insert(
+        HealthDBData(
+          name: e.key,
+          value: e.value,
+          time: calendar.dateTimeToInt(dateTime),
+          timeModified: DateTime.now().millisecondsSinceEpoch,
+        ),
+      );
+    }
+
+    healthData.addAll(additionalHealthData);
+
     return healthData;
   }
 }
