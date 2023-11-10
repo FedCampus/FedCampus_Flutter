f316ae8b12368d932d46839b64e930032fc423c1
Luyao Wang <lywlywly@gmail.com>
2023-11-11 03:26:37 +0800

    better encapsulation

--- fedcampus/lib/models/health_data_model.dart
+++ fedcampus/lib/models/health_data_model.dart
@@ -11,14 +11,15 @@ import '../utility/event_bus.dart';
 import '../utility/my_exceptions.dart';
 
 class HealthDataModel extends ChangeNotifier {
-  Map<String, double> healthData = HealthData.mapOf();
-  Map<String, double> screenData = ScreenData.mapOf();
-  bool isAuth = false;
+  final Map<String, double> _healthData = HealthData.mapOf();
+  final Map<String, double> _screenData = ScreenData.mapOf();
   bool _loading = false;
   String _date = (DateTime.now().year * 10000 +
           DateTime.now().month * 100 +
           DateTime.now().day)
       .toString();
+
+  Map<String, double> get healthData => Map.unmodifiable(_healthData);
 
   String get date => _date;
 
@@ -34,10 +35,13 @@ class HealthDataModel extends ChangeNotifier {
   Future<void> requestAllData({bool forcedRefresh = false}) async {
     _loading = true;
     notifyListeners();
-    healthData = HealthData.mapOf();
+    // reset each entry to 0 whenever a new request is received.
+    for (var k in _healthData.keys) {
+      _healthData[k] = 0;
+    }
 
     try {
-      healthData.addAll(await _getBodyData(forcedRefresh: forcedRefresh));
+      _healthData.addAll(await _getBodyData(forcedRefresh: forcedRefresh));
     } on AuthenticationException catch (error) {
       logger.e(error);
       _notify();
@@ -52,7 +56,7 @@ class HealthDataModel extends ChangeNotifier {
 
     if (userApi.isAndroid) {
       try {
-        healthData.addAll(await _getScreenData());
+        _healthData.addAll(await _getScreenData());
       } catch (e) {
         logger.e(e);
         _notify();
@@ -76,10 +80,9 @@ class HealthDataModel extends ChangeNotifier {
             .intToDateTime(int.parse(date))
             .add(const Duration(days: 1)));
 
-    screenData.addAll(res);
-    logger.i(screenData);
-    healthData.addAll(screenData);
-    return screenData;
+    _screenData.addAll(res);
+    logger.i(_screenData);
+    return _screenData;
   }
 
   Future<Map<String, double>> _getBodyData({bool forcedRefresh = false}) async {
