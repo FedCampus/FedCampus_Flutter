ef8076de68e2a6151a0a26b6db2d1cc4fee2d701
Beilong-Tang <bt132@duke.edu>
2023-11-27 19:38:52 +0800

    add email send

--- fa_backend/send_email/main.py
+++ fa_backend/send_email/main.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import os
+import argparse
+import sys
 from django.core.mail import send_mass_mail
 
-base_path = os.path.join(os.getcwd(), "send_email")
+##
+sys.path.append(os.path.dirname(os.getcwd()))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fedapp.settings")
 
 
 def sendmail(
     content, netid, subject, from_email="fedcampus@dukekunshan.edu.cn", test=False
 ):
     if test:
-        # netid = ["bt132", "sh623", "qz150", "lw337", "bz106", "bl291"]
-        netid = ["bt132"]
-        content = content[3:4]
+        # print("testing")
+        netid = ["bt132", "sh623", "qz150", "lw337", "bz106", "bl291"]
+        # netid = ["bt132"]
     assert len(content) == len(netid)
     send_mass_mail(
         ((subject, c, from_email, [n + "@duke.edu"]) for c, n in zip(content, netid))
@@ -19,19 +23,29 @@ def sendmail(
     pass
 
 
-def main():
-    f = open(os.path.join(base_path, "box.txt"), "r").read()
-    p = pd.read_excel(os.path.join(base_path, "FecCampus Watches.xlsx"))
-    p = p[p["Watch Serial Number"].notnull()]
-    name = p["name"]
-    serial = p["Watch Serial Number"]
+def main(args):
+    f = open("reminder.txt", "r").read()
+
+    p = pd.read_excel("FecCampus Watches.xlsx")
+    p = p[p["Watch Serial Number"].isnull()]
+    p = p[p["Checked"] != 0]
+    p = p[p["Name"].notnull()]
+    name = p["Name"]
     netid = p["NetID"]
-    messages = [f % (n, s) for n, s in zip(name, serial)]
-    sendmail(
-        messages, netid, "Congratulations of being a Fedcampus Test users!", test=False
-    )
+    messages = [f % s for s in name]
+    if args.email:
+        send = True if args.email == "send" else False
+        sendmail(
+            messages,
+            netid,
+            "Last Chance to get your Huawei Watches!",
+            test=not send,
+        )
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--email", default="test")
+    args = parser.parse_args()
+    main(args)
     pass
