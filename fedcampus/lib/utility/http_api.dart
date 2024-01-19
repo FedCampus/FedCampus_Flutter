3bf0d085ce32488444a0f77bf430ca2f471a9e64
Luyao Wang <lywlywly@gmail.com>
2024-01-19 18:47:22 +0800

    change back to deployment port

--- fedcampus/lib/utility/http_api.dart
+++ fedcampus/lib/utility/http_api.dart
@@ -10,7 +10,7 @@ import 'package:fedcampus/utility/global.dart';
 
 class HTTPApi {
   // TODO: make the _host in the production mode
-  static const _host = "http://10.201.8.29:8005/";
+  static const _host = "http://10.201.8.29:8006/";
 
   static const login = "${_host}api/login";
 
