c5e8642771a552bc81564a0647d62c78b5276a89
Luyao Wang <lywlywly@gmail.com>
2024-01-20 19:49:25 +0800

    document health_handler.dart

--- fedcampus/lib/models/datahandler/health_handler.dart
+++ fedcampus/lib/models/datahandler/health_handler.dart
@@ -4,9 +4,13 @@ import '../../utility/health_database.dart';
 import '../../utility/log.dart';
 import '../../utility/calendar.dart' as calendar;
 
-/// Abstract class for health data controller on variery of platforms such as Huawei Health, Google Fit.
-/// The interface provides methods to get [Data] from an interval or a day; it also supports get a list of [Data] from a list of types.
-/// The implementations are [List] centered, and [Map] implementations are based on [List], which is inspired from Flutter [Health](https://pub.dev/packages/health)
+/// The abstract class provided serves as a health data controller that facilitates the integration of health data from different platforms, including Huawei Health and Google Fit.
+/// It provides a set of methods that enable the retrieval of data within a specific time interval or for a particular day.
+/// Additionally, it supports obtaining a list of data by specifying the desired types.
+///
+/// The implementations of this abstract class primarily utilize lists as the underlying data structure.
+/// Map implementations are derived from these lists, allowing for efficient data retrieval and manipulation.
+/// The design and structure of this abstract class were influenced by Flutter [Health](https://pub.dev/packages/health)
 class FedHealthData {
   /// `false` by default.
   bool get canAuth => false;
@@ -24,6 +28,7 @@ class FedHealthData {
     throw UnimplementedError();
   }
 
+  /// Retrieves data for the given [entry] between the [startTime] and [endTime].
   Future<Data> getDataInterval({
     required String entry,
     required DateTime startTime,
@@ -32,6 +37,7 @@ class FedHealthData {
     throw UnimplementedError();
   }
 
+  /// Gets data of type [entry] on [date].
   Future<Data> getDataDay({
     required String entry,
     required DateTime date,
@@ -42,8 +48,10 @@ class FedHealthData {
     return data;
   }
 
-  /// ~~when some entries went wrong, this method return other fine entries~~
-  /// when some entries went wrong, this method throws [Exception] even if some succeeds
+  /// Retrieves data for each type in [entry] within the time range defined by [startTime] and [endTime].
+  /// Returns a list of [Data] objects representing the retrieved data.
+  ///
+  /// Throws an [Exception] if any individual data retrieval fails, even if some succeed.
   Future<List<Data>> getDataListInterval({
     required List<String> entry,
     required DateTime startTime,
@@ -66,19 +74,23 @@ class FedHealthData {
     throw UnimplementedError();
   }
 
-  /// first try to retrieve from database. If time is today, only allow 30 min cache, otherwise always use cached data unless forced refresh.
-  /// ~~when some entries went wrong, this method return other fine entries.~~
-  /// when some entries went wrong, this method throws [Exception] even if some succeeds.
-  /// children of this class should throw [AuthenticationException] if not authenticated, which will cause [HealthDataModel] to call `authenticate()`.
+  /// Retrieves data from the database. If the time is today, it allows only 30 minutes cache,
+  /// otherwise, it always uses cached data unless [forcedRefresh] is enabled.
   ///
-  /// subclass implementation:
+  /// When some entries encounter errors, this method throws an [Exception] even if some succeed.
+  ///
+  /// Subclasses should throw an [AuthenticationException] if not authenticated,
+  /// which will cause [HealthDataModel] to call `authenticate()`.
+  ///
+  /// Subclass implementation:
   ///   1. Huawei: good
-  ///   2. Google: not perfect because FLutter Health only throws exceptions when the data is not available on the platform,
-  ///   but when the data is not rqeusted by [requestAuthorization], Flutter Health does not throw exceptions.
-  ///   Specifically, native code prints the error but the method channel does not throw a [PlatformException] in ways like `callback(Result.failure(err))`,
-  ///   and what we can see is just logging info in the console.
-  ///   Therefore, GoogleFit has to call `authenticate()` upon instantiation. Luckily, unlike Huawei, this process does not redirect to another page, which is acceptable.
-  ///   3. iOS: TODO
+  ///   2. Google: not perfect because Flutter Health only throws exceptions when the data is not available on the platform,
+  ///      but when the data is not requested by [requestAuthorization], Flutter Health does not throw exceptions.
+  ///      Specifically, native code prints the error, but the method channel does not throw a [PlatformException] in ways like `callback(Result.failure(err))`,
+  ///      and what we can see is just logging info in the console.
+  ///      Therefore, GoogleFit has to call `authenticate()` upon instantiation.
+  ///      Luckily, unlike Huawei, this process does not redirect to another page, which is acceptable.
+  ///   3. iOS: good
   Future<List<Data>> getCachedDataListDay(List<String> nameList, int time,
       {bool forcedRefresh = false}) async {
     DateTime dateTime = calendar.intToDateTime(time);
@@ -142,8 +154,7 @@ class FedHealthData {
             1800000);
   }
 
-  /// ~~when some entries went wrong, this method return other fine entries~~
-  /// when some entries went wrong, this method throws [Exception] even if some succeeds
+  /// When some entries encounter errors, this method throws an [Exception] even if some succeed.
   Future<Map<String, double>> getCachedValueMapDay(
       DateTime dateTime, List<String> dataList,
       {bool forcedRefresh = false}) async {
