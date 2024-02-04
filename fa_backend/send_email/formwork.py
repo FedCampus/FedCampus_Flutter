c3634dd287c97f4733d51a34957ec02873005658
Luyao Wang <lywlywly@gmail.com>
2024-02-04 17:29:23 +0800

    format formwork.py

--- fa_backend/send_email/formwork.py
+++ fa_backend/send_email/formwork.py
@@ -8,6 +8,7 @@ from django.core.mail import send_mass_mail
 sys.path.append(os.path.dirname(os.getcwd()))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fedapp.settings")
 
+
 def filter_users(excel_file, grade_exclusion, status_required):
     """
     Filters users from the Excel file based on the specified criteria.
@@ -21,10 +22,13 @@ def filter_users(excel_file, grade_exclusion, status_required):
     - A filtered DataFrame with users meeting the criteria.
     """
     df = pd.read_excel(excel_file, sheet_name="waitlist")
-    df_filtered = df #to be modified
+    df_filtered = df  # to be modified
     return df_filtered
 
-def sendmail(content, netid, subject, from_email="fedcampus@dukekunshan.edu.cn", test=False):
+
+def sendmail(
+    content, netid, subject, from_email="fedcampus@dukekunshan.edu.cn", test=False
+):
     """
     Sends an email to each user in the netid list with their corresponding content.
 
@@ -40,7 +44,10 @@ def sendmail(content, netid, subject, from_email="fedcampus@dukekunshan.edu.cn",
         netid = test
         content = content[: len(netid)]
     assert len(content) == len(netid), "The length of content and netid must match."
-    send_mass_mail(((subject, c, from_email, [n + "@duke.edu"]) for c, n in zip(content, netid)))
+    send_mass_mail(
+        ((subject, c, from_email, [n + "@duke.edu"]) for c, n in zip(content, netid))
+    )
+
 
 def main(args):
     # Read the waitlist text content
@@ -48,7 +55,9 @@ def main(args):
         waitlist_content = file.read()
 
     # Filter users based on criteria
-    users_df = filter_users(args.excel_file, grade_exclusion=2024, status_required="student")
+    users_df = filter_users(
+        args.excel_file, grade_exclusion=2024, status_required="student"
+    )
     messages = [waitlist_content % s for s in users_df["Name"]]
     netids = users_df["NetID"]
 
@@ -60,10 +69,24 @@ def main(args):
         send = True if args.email_action == "send" else False
         sendmail(messages, netids, subject, test=not send)
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Send emails to filtered users from an Excel file.")
-    parser.add_argument("--excel_file", required=True, help="The path to the Excel file.")
-    parser.add_argument("--text_file", required=True, help="The path to the text file with the email content.")
-    parser.add_argument("--email_action", choices=["test", "send"], default="test", help="Whether to test or send emails.")
+    parser = argparse.ArgumentParser(
+        description="Send emails to filtered users from an Excel file."
+    )
+    parser.add_argument(
+        "--excel_file", required=True, help="The path to the Excel file."
+    )
+    parser.add_argument(
+        "--text_file",
+        required=True,
+        help="The path to the text file with the email content.",
+    )
+    parser.add_argument(
+        "--email_action",
+        choices=["test", "send"],
+        default="test",
+        help="Whether to test or send emails.",
+    )
     args = parser.parse_args()
     main(args)
