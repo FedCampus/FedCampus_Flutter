52be5b027578ccb08ad1bf6ff93a9c16bfd018ae
theta <theta.lin@yandex.com>
2024-02-04 12:51:47 +0800

    remove redundant comment for FA_MODEL, replacing it with an explanation of the variable's intention

--- fa_backend/api/views.py
+++ fa_backend/api/views.py
@@ -130,7 +130,7 @@ class saveLogFile(APIView):
     def post(self, request):
         file = request.data.get("log")
 
-        ## TODO : change the max count of the log file for each user
+        # TODO : change the max count of the log file for each user
         temp_max_count = 2
 
         if (
@@ -145,7 +145,7 @@ class saveLogFile(APIView):
         return Response(None)
 
 
-## TODO: change the FA model back to RecordDP
+# For easily switching between Record and RecordDP since they share the same schema
 FA_MODEL = RecordDP
 
 
@@ -285,7 +285,7 @@ class DPDataPoints(APIView):
 
 
 def getSimilarUser(querySet, index, length=1):
-    ## get the similar user that is similar to the current user
+    # get the similar user that is similar to the current user
     result = []
     indexLength = querySet.count() - 1
     lowerBound = max(index - length, 0)
