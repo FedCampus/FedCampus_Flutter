0cdee8343f9346dafeb6d34f9a89d5a8c506795d
Luyao Wang <lywlywly@gmail.com>
2024-02-04 17:10:16 +0800

    add customer serializer test

--- fa_backend/api/tests.py
+++ fa_backend/api/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .serializers import RegisterSerializer
 
-# Create your tests here.
+
+class DummyTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_dummy(self):
+        self.assertEqual("Dummy Test", "Dummy Test")
+
+
+class CustomerTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_customer_serializer_01(self):
+        serializer = RegisterSerializer(
+            data={
+                "email": "lw337@duke.edu",
+                "password": "testpasswd",
+                "netid": "lw337",
+            }
+        )
+        self.assertEqual(serializer.is_valid(), True)
